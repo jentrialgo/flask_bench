e1effbdb68da5a07f088fad5b8b0844d6a151332
Joaquín Entrialgo <joaquin@uniovi.es>
2022-11-25 09:38:25 +0100

    Use 2*ncpus+1 as the number of workers for gunicorn

--- server/gunicorn.sh
+++ server/gunicorn.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-gunicorn --chdir /app app:app -w 2 --threads 2 -b 0.0.0.0:80
+NUM_CPUS=$(nproc)
+NUM_WORKERS=$((2 * $NUM_CPUS + 1))
+gunicorn --chdir /app app:app -w $NUM_WORKERS -b 0.0.0.0:80
