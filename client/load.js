f1ed8008a34c6142cd3eb06e88ed50085d193f1d
Joaquín Entrialgo <joaquin@uniovi.es>
2022-11-28 10:15:45 +0100

    Fix bug in load.js

--- client/load.js
+++ client/load.js
@@ -5,10 +5,10 @@ import http from 'k6/http';
 // - NUM_IT: number of iterations to execute in the server
 //
 // Example:
-//   k6 run -e SERVER=1.2.3.4 -e NUM_IT=53321
+//   k6 run -e SERVER=1.2.3.4 -e NUM_IT=53321 --duration=30s --vus=10 load.js
 
 export default function () {
-  server = __ENV.SERVER
-  num_it = __ENV.NUM_IT
+  let server = __ENV.SERVER;
+  let num_it = __ENV.NUM_IT;
   http.get(`http://${server}?it1=${num_it}&it2=1`);
 }
